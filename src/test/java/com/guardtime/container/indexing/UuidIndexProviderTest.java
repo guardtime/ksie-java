d91047ae6d51e0be10961c71ceec1f420040455a
Tiit Kass <tiit.kass@guardtime.com>
2016-09-22 14:52:22 +0300

    Added RFC4122 index provider implementation

---
+++ src/test/java/com/guardtime/container/indexing/UuidIndexProviderTest.java
@@ -0,0 +1,25 @@
+package com.guardtime.container.indexing;
+
+import com.guardtime.container.AbstractContainerTest;
+import com.guardtime.container.packaging.Container;
+import com.guardtime.container.packaging.zip.ZipContainerPackagingFactory;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.Arrays;
+
+public class UuidIndexProviderTest extends AbstractContainerTest {
+
+    private IndexProvider indexProvider = new UuidIndexProvider();
+
+    @Test
+    public void testUpdateWithInvalidContainer() throws Exception {
+        expectedException.expect(IndexingException.class);
+        expectedException.expectMessage("Not a RFC4122 UUID based index");
+
+        ZipContainerPackagingFactory packagingFactory = new ZipContainerPackagingFactory(mockedSignatureFactory, mockedManifestFactory, Mockito.mock(IndexProvider.class));
+        Container container = packagingFactory.create(Arrays.asList(TEST_DOCUMENT_HELLO_TEXT), Arrays.asList(MOCKED_ANNOTATION));
+        indexProvider.updateIndexes(container);
+    }
+
+}
