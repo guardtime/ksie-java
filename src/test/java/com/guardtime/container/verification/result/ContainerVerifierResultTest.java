c9d2248fce65141f5c5ad27aad9d399134f34857
Tiit Kass <tiit.kass@guardtime.com>
2016-10-17 17:03:42 +0300

    Added a missing test

--- src/test/java/com/guardtime/container/verification/result/ContainerVerifierResultTest.java
+++ src/test/java/com/guardtime/container/verification/result/ContainerVerifierResultTest.java
@@ -1,15 +1,22 @@
 package com.guardtime.container.verification.result;
 
+import com.guardtime.container.manifest.Manifest;
+import com.guardtime.container.manifest.SignatureReference;
 import com.guardtime.container.packaging.Container;
+import com.guardtime.container.packaging.SignatureContent;
+import com.guardtime.container.util.Pair;
 import com.guardtime.container.verification.rule.Rule;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
+import java.util.Arrays;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.doReturn;
 
 public class ContainerVerifierResultTest {
     private static final Container MOCK_CONTAINER = Mockito.mock(Container.class);
@@ -39,6 +46,24 @@ public class ContainerVerifierResultTest {
     public void getContainer() throws Exception {
         assertNotNull(result.getContainer());
         assertEquals(MOCK_CONTAINER, result.getContainer());
+    }
+
+    @Test
+    public void getSignatureResult() throws Exception {
+        String path = "signature.ksig";
+        SignatureContent mockSignatureContent = Mockito.mock(SignatureContent.class);
+        Manifest mockManifest = Mockito.mock(Manifest.class);
+        SignatureReference mockSignatureReference = Mockito.mock(SignatureReference.class);
+
+        doReturn(Arrays.asList(mockSignatureContent)).when(MOCK_CONTAINER).getSignatureContents();
+        doReturn(Pair.of("str", mockManifest)).when(mockSignatureContent).getManifest();
+        doReturn(mockSignatureReference).when(mockManifest).getSignatureReference();
+        doReturn(path).when(mockSignatureReference).getUri();
+
+        RESULT_HOLDER.setSignatureResult(path, Mockito.mock(SignatureResult.class));
+        ContainerVerifierResult result = new ContainerVerifierResult(MOCK_CONTAINER, RESULT_HOLDER);
+
+        assertNotNull(result.getSignatureResult(mockSignatureContent));
     }
 
 }
