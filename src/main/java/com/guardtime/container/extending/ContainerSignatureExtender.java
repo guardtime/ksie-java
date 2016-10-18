246e5ca2445f63346e6d40914a17fe497cd04966
Tiit Kass <tiit.kass@guardtime.com>
2016-10-18 15:17:20 +0300

    Added feedback from extending process

--- src/main/java/com/guardtime/container/extending/ContainerSignatureExtender.java
+++ src/main/java/com/guardtime/container/extending/ContainerSignatureExtender.java
@@ -6,6 +6,8 @@ import com.guardtime.container.packaging.SignatureContent;
 import com.guardtime.container.signature.ContainerSignature;
 import com.guardtime.container.signature.SignatureException;
 import com.guardtime.container.signature.SignatureFactory;
+import com.guardtime.container.util.Util;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,6 +20,8 @@ public class ContainerSignatureExtender {
     private final ExtendingPolicy policy;
 
     public ContainerSignatureExtender(SignatureFactory signatureFactory, ExtendingPolicy policy) {
+        Util.notNull(signatureFactory, "Signature factory");
+        Util.notNull(policy, "Extending policy");
         this.signatureFactory = signatureFactory;
         this.policy = policy;
     }
@@ -26,17 +30,27 @@ public class ContainerSignatureExtender {
      * Extends each signature in input container and returns the container.
      * If a signature extending fails it is logged at INFO level and skipped.
      * @param container    Container to be extended.
+     * @return True if all signatures were extended successfully, false otherwise.
      */
-    public void extend(Container container) {
+    public boolean extend(Container container) {
+        boolean status = true;
         for (SignatureContent content : container.getSignatureContents()) {
+            Manifest manifest = content.getManifest().getRight();
+            String signatureUri = manifest.getSignatureReference().getUri();
             try {
-                signatureFactory.extend(content.getContainerSignature(), policy);
+                ContainerSignature containerSignature = content.getContainerSignature();
+                signatureFactory.extend(containerSignature, policy);
+
+                if (!containerSignature.isExtended()) {
+                    status = false;
+                    logger.info("Extending signature '{}' resulted in a non-extended signature without exception!", signatureUri);
+                }
             } catch (SignatureException e) {
-                Manifest manifest = content.getManifest().getRight();
-                String signatureUri = manifest.getSignatureReference().getUri();
+                status = false;
                 logger.info("Failed to extend signature '{}' because: '{}'", signatureUri, e.getMessage());
             }
         }
+        return status;
     }
 
 }
