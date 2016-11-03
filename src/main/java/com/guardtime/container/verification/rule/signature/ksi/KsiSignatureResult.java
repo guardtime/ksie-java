bec1f36d7dcf232d23213f4a756e0b71e09f916d
Tiit Kass <tiit.kass@guardtime.com>
2016-11-03 13:01:28 +0200

    Reverted KsiSignatureResult access

--- src/main/java/com/guardtime/container/verification/rule/signature/ksi/KsiSignatureResult.java
+++ src/main/java/com/guardtime/container/verification/rule/signature/ksi/KsiSignatureResult.java
@@ -4,12 +4,12 @@ import com.guardtime.container.verification.result.SignatureResult;
 import com.guardtime.ksi.unisignature.KSISignature;
 import com.guardtime.ksi.unisignature.verifier.VerificationResult;
 
-class KsiSignatureResult implements SignatureResult<KSISignature, VerificationResult> {
+public  class KsiSignatureResult implements SignatureResult<KSISignature, VerificationResult> {
     private final VerificationResult result;
     private final com.guardtime.container.verification.result.VerificationResult simpleResult;
     private final KSISignature signature;
 
-    KsiSignatureResult(VerificationResult result, com.guardtime.container.verification.result.VerificationResult simpleResult, KSISignature signature) {
+    public KsiSignatureResult(VerificationResult result, com.guardtime.container.verification.result.VerificationResult simpleResult, KSISignature signature) {
         this.result = result;
         this.simpleResult = simpleResult;
         this.signature = signature;
