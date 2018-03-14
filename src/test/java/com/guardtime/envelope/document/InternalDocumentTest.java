7121a4a4149c47be31bd2fd415a22eb85b784350
Margo.Raja <margo.raja@guardtime.com>
2018-03-14 16:09:38 +0200

    Minor test clean up.

--- src/test/java/com/guardtime/envelope/document/InternalDocumentTest.java
+++ src/test/java/com/guardtime/envelope/document/InternalDocumentTest.java
@@ -2,20 +2,28 @@ package com.guardtime.envelope.document;
 
 import com.guardtime.envelope.AbstractEnvelopeTest;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.Arrays;
 
+import static com.guardtime.ksi.util.Util.toByteArray;
 import static junit.framework.TestCase.assertTrue;
 
 public class InternalDocumentTest extends AbstractEnvelopeTest {
 
     @Test
     public void testElementNull() {
+        //TODO: Add expected results.
         new InternalDocument(null);
+    }
+
+    @Test
+    public void testNullFileName() {
+        //TODO: Add expected results.
+        AbstractDocument mockDoc = Mockito.mock(AbstractDocument.class);
+        new InternalDocument(mockDoc);
     }
 
     @Test
@@ -23,23 +31,11 @@ public class InternalDocumentTest extends AbstractEnvelopeTest {
         StreamDocument document = new StreamDocument(new ByteArrayInputStream(new byte[32]), "doc", "doc");
         InternalDocument internalDocument = new InternalDocument(document);
 
-        assertTrue(isEqual(document.getInputStream(), internalDocument.getInputStream()));
-    }
-
-    private boolean isEqual(InputStream i1, InputStream i2) throws IOException {
-        return Arrays.equals(getBytes(i1), getBytes(i2));
-    }
-
-    private byte[] getBytes(InputStream stream) throws IOException {
-        try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
-            int read;
-            byte[] data = new byte[64];
-
-            while ((read = stream.read(data, 0, data.length)) != -1) {
-                baos.write(data, 0, read);
-            }
-
-            return baos.toByteArray();
-        }
+        assertTrue(
+                Arrays.equals(
+                        toByteArray(document.getInputStream()),
+                        toByteArray(internalDocument.getInputStream())
+                )
+        );
     }
 }
